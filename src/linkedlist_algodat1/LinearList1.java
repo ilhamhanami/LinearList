de566357ec209457908709d05e540fbb49238b89
ilhamhanami <asus@192.168.56.1>
2017-10-10 21:23:17 +0700

    add circular list and double linked list

---
+++ src/linkedlist_algodat1/LinearList1.java
@@ -0,0 +1,10 @@
+package linkedlist_algodat1;
+
+public interface LinearList1 {
+    public boolean isEmpty();
+    public Object get(int index);
+    public int indexOf(Object element);
+    public Object remove(int index);
+    public void add(int index,Object obj);
+    public String toString();
+}
